555ccbc0f150ac3a37aa67c8f8dcc6fcd3e3f7b1
Ross Yeager <ryeager12@gmail.com>
2023-04-15 22:21:03 -0700

    [submit] reset fields after submitting Topic: reset-fields Relative: helper-hooks

--- src/components/Submit.tsx
+++ src/components/Submit.tsx
@@ -3,19 +3,13 @@ import Button from "@mui/material/Button";
 import "./Styles.css";
 
 const Submit: React.FC<any> = (props: any) => {
-  const { handleSubmit, ...additionalProps } = props;
+  const { styleProps, handleSubmit, ...additionalProps } = props;
   return (
     <Button
       onClick={handleSubmit}
       variant="text"
       color="secondary"
-      style={{
-        borderRadius: 25,
-        backgroundColor: "#ffffff",
-        padding: "3mm 3mm",
-        fontSize: "1em",
-        fontWeight: "bold",
-      }}
+      style={styleProps}
       {...additionalProps}
     >
       Submit Deal
